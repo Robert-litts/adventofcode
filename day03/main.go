0c8736d3a058aa62afd4183027f018ac010ac261
Robbie <Robert@litts.me>
2024-12-04 16:18:09 -0500

    update Day 3 part 2 solution

--- day03/main.go
+++ day03/main.go
@@ -10,18 +10,18 @@ import (
 )
 
 func main() {
-	var inputFile = flag.String("inputFile", "../input/day3.example", "Relative file path to use as input.")
+	var inputFile = flag.String("inputFile", "../input/day3.input", "Relative file path to use as input.")
 	flag.Parse()
 	fmt.Println("Running Part 1:")
 	if err := Part1(*inputFile); err != nil {
 		fmt.Println("Error in Part 2:", err)
 		return
 	}
-	// fmt.Println("Running Part 2:")
-	// if err := Part2(*inputFile); err != nil {
-	// 	fmt.Println("Error in Part 2:", err)
-	// 	return
-	// }
+	fmt.Println("Running Part 2:")
+	if err := Part2(*inputFile); err != nil {
+		fmt.Println("Error in Part 2:", err)
+		return
+	}
 }
 
 func Part1(inputFile string) error {
@@ -60,5 +60,73 @@ func Part1(inputFile string) error {
 		}
 	}
 	fmt.Println("Result:", result)
+	return nil
+}
+
+func Part2(inputFile string) error {
+	bytes, err := os.ReadFile(inputFile)
+	if err != nil {
+		return err
+	}
+	result := 0
+	contents := string(bytes)
+	pattern_dont := `(?:^|[^a-zA-Z])don't\(\)`
+	pattern_mul := `mul\((-?\d+(\.\d+)?),(-?\d+(\.\d+)?)\)`
+	pattern_do := `do\(\)`
+	re_dont := regexp.MustCompile(pattern_dont)
+	re_mul := regexp.MustCompile(pattern_mul)
+	re_do := regexp.MustCompile(pattern_do)
+	do := true
+
+	index_dont := re_dont.FindAllStringIndex(contents, -1)
+	index_mul := re_mul.FindAllStringIndex(contents, -1)
+	index_do := re_do.FindAllStringIndex(contents, -1)
+	dont_index := 0
+	do_index := 0
+	mul_index := 0
+
+	// loop thru each char in the line until the end
+	for i := 0; i < len(contents); {
+
+		//If we reach a dont,
+		if dont_index < len(index_dont) && i == index_dont[dont_index][0] {
+			do = false
+			i = index_dont[dont_index][1]
+			dont_index++
+			continue
+		}
+
+		//If we reach a do
+		if do_index < len(index_do) && i == index_do[do_index][0] {
+			do = true
+			i = index_do[do_index][1]
+			do_index++
+			continue
+		}
+
+		// Check for mul()
+		if mul_index < len(index_mul) && i == index_mul[mul_index][0] {
+			//If do is enabled
+			if do {
+				mulMatch := re_mul.FindStringSubmatch(contents[index_mul[mul_index][0]:index_mul[mul_index][1]])
+				if len(mulMatch) > 0 {
+					//fmt.Println("MulMatch: ", mulMatch)
+					var x, y int
+					fmt.Sscanf(mulMatch[1], "%d", &x)
+					fmt.Sscanf(mulMatch[3], "%d", &y)
+					result += x * y
+					//fmt.Println("Result: ", result)
+				}
+			}
+			//If do is disabled, skip the mull...move i to the end of current mul & increment mul_index to the next mul
+			i = index_mul[mul_index][1]
+			mul_index++
+			continue
+		}
+		i++
+
+	}
+
+	fmt.Println("Result Part 2:", result)
 	return nil
 }
