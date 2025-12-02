00a36198a98c4531ac0fcd562abb68b0746fad1a
Robbie <Robert@litts.me>
2025-12-02 11:00:50 -0500

    2025 day 1, part 2 solution

--- 2025/day01/main.go
+++ 2025/day01/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	var inputFile = flag.String("inputFile", "../input/day1.ex.input", "Relative file path to use as input.")
+	var inputFile = flag.String("inputFile", "../input/day1.input", "Relative file path to use as input.")
 	flag.Parse()
 	fmt.Println("Running Part 1:")
 	if err := Part1(*inputFile); err != nil {
@@ -41,9 +41,7 @@ func Part1(inputFile string) error {
 	for _, line := range lines {
 		direction := re.FindAllString(line, -1)[0]
 		turns := re.Split(line, -1)
-		fmt.Println(direction)
 		numStr := turns[1]
-		fmt.Println(numStr)
 
 		num_turns, err := strconv.Atoi(numStr)
 		if err != nil {
@@ -52,8 +50,6 @@ func Part1(inputFile string) error {
 		if num_turns > 99 {
 			num_turns = num_turns % 100
 		}
-		fmt.Println(num_turns)
-
 		if direction == "R" {
 			position = position + num_turns
 		} else {
@@ -69,9 +65,8 @@ func Part1(inputFile string) error {
 		if position == 0 {
 			password += 1
 		}
-		fmt.Println("Position: ", position)
+		//fmt.Println("Position: ", position)
 	}
-	//}
 	fmt.Println("Result:", password)
 	return nil
 }
@@ -81,15 +76,60 @@ func Part2(inputFile string) error {
 	if err != nil {
 		return err
 	}
-	result := 0
-
 	contents := string(bytes)
 	lines := strings.Split(contents, "\n")
+	pattern := `[RL]`
+	re := regexp.MustCompile(pattern)
+
+	start_position := 50
+	position := 50
+	password := 0
+	zero_passed := 0
 
 	for _, line := range lines {
-		fmt.Println(line)
+		direction := re.FindAllString(line, -1)[0]
+		turns := re.Split(line, -1)
+		numStr := turns[1]
+		num_turns, err := strconv.Atoi(numStr)
+		if err != nil {
+			fmt.Println("Error converting string to int:", err)
+		}
+		//Handle large turn values
+		if num_turns > 99 {
+			zero_passed += num_turns / 100 //retain total number full turns completed
+			num_turns = num_turns % 100    //reduce to within a single turn via modulo
+		}
 
-		fmt.Println("Result Part 2:", result)
+		if direction == "R" {
+			position = position + num_turns
+		} else {
+			position = position - num_turns
+		}
+
+		if position > 99 {
+			position = position - 100
+
+			//Handle case where it doesn't land on 0, but also didnt start on 0
+			if position != 0 && start_position != 0 {
+				zero_passed += 1
+			}
+		} else if position < 0 {
+			position = 100 + position
+
+			//Handle case where it doesn't land on 0, but also didnt start on 0 (to avoid double counting)
+			if position != 0 && start_position != 0 {
+				zero_passed += 1
+			}
+
+		}
+		//Handle case where it stops on 0
+		if position == 0 {
+			password += 1
+		}
+		//reset starting position
+		start_position = position
 	}
+	total_score := password + zero_passed
+	fmt.Println("Result:", total_score)
 	return nil
 }
